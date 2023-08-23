f6cf9908197e15c249302c3e72fc86d55d08c0d5
jose zambrano <jose.zambrano@sinapsiscorp.com>
2023-08-23 15:47:00 -0500

    change manual arm64

--- sam-app-5/hello-world/app.ts
+++ sam-app-5/hello-world/app.ts
@@ -16,7 +16,7 @@ export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGat
         response = {
             statusCode: 200,
             body: JSON.stringify({
-                message: 'hello world test config x86_64',
+                message: 'hello world test config manual arm64',
             }),
         };
     } catch (err: unknown) {
